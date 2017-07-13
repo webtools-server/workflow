36117831bc598a36df2c71cbe6f83d5693a16d9b
canye <canye.wu@jyblife.com>
2017-07-13 11:59:34 +0800

    feat(jfet-build): 修改查找preset的顺序

--- packages/jfet-build/lib/constant.js
+++ packages/jfet-build/lib/constant.js
@@ -3,5 +3,5 @@
  */
 
 module.exports = {
-    PRESET_PREFIX: ['jfet-build-preset-', '@jyb/jfet-build-preset-']
+    PRESET_PREFIX: ['@jyb/jfet-build-preset-', 'jfet-build-preset-']
 };
--- packages/jfet-build/lib/util/get_module.js
+++ packages/jfet-build/lib/util/get_module.js
@@ -2,6 +2,7 @@
  * 获取模块
  */
 
+const path = require('path');
 const utilLog = require('./log');
 
 function getModule(resources) {
@@ -10,7 +11,14 @@ function getModule(resources) {
 
     for (let i = 0, l = resources.length; i < l; i++) {
         try {
-            result = require(resources[i]);
+            const curr = resources[i];
+            const modulePaths = [path.join(__dirname, '..', '..', 'node_modules', curr), curr];
+
+            // 按照当前插件node_modules，全局下安装的node_modules顺序找到模块
+            for (let m = 0, n = modulePaths.length; m < n; m++) {
+                result = require(modulePaths[i]);
+                return result;
+            }
         } catch (e) {
             err.push(e);
         }
