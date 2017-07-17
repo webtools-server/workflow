b618be714600906642c17161b930be7c18f8acef
canye <canye.wu@jyblife.com>
2017-07-17 13:00:57 +0800

    feat: assets去除fileType的支持

--- packages/jfet-build-block-assets/lib/file.js
+++ packages/jfet-build-block-assets/lib/file.js
@@ -1,20 +1,12 @@
-const { synthesizeMatch } = require('./compat');
 
 /**
- * @param {string} [fileType]   Deprecated.
  * @param {object} [options] You can pass all file-loader options.
  * @return {Function}
  * @see https://github.com/webpack-contrib/file-loader
  */
-function file(fileType, options = {}) {
+function file(options = {}) {
     return (context, util) => {
-        if (fileType && typeof fileType === 'object' && Object.keys(options).length === 0) {
-            options = fileType;
-            fileType = null;
-        }
-        if (fileType || options.exclude || options.include) {
-            console.warn('file(): You are using the deprecated \'fileType\' parameter, \'options.exclude\' or \'options.include\'. Use match() instead.');
-        } else if (!context.match) {
+        if (!context.match) {
             throw new Error(
                 'The file() block can only be used in combination with match(). ' +
                 'Use match() to state on which files to apply the file loader.'
@@ -24,11 +16,12 @@ function file(fileType, options = {}) {
         return util.addLoader(
             Object.assign({
                 use: [
-                    { loader: 'file-loader', options }
+                    {
+                        loader: require.resolve('file-loader'),
+                        options
+                    }
                 ]
             },
-                // for API backwards compatibility only
-            synthesizeMatch(fileType && context.fileType(fileType), options),
             context.match
             )
         );
--- packages/jfet-build-block-assets/lib/index.js
+++ packages/jfet-build-block-assets/lib/index.js
@@ -1,9 +1,11 @@
 const css = require('./css');
 const file = require('./file');
 const url = require('./url');
+const image = require('./image');
 
 module.exports = {
     css,
     file,
-    url
+    url,
+    image
 };
