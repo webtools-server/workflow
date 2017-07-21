357a3c9ded0cd36cdef03cae869803aa4c3d1072
canye <canye.wu@jyblife.com>
2017-07-21 14:24:59 +0800

    fix(block dot): 修正dot block图片问题

--- packages/jfet-build-block-dot/lib/loader/dot.js
+++ packages/jfet-build-block-dot/lib/loader/dot.js
@@ -7,13 +7,27 @@ const dot = require('dot');
 const fs = require('fs');
 
 function dotLoader() {
-    const options = loaderUtils.getOptions(this);
-
-    Object.assign(dot.templateSettings, options);
+    let result = '';
+    const options = loaderUtils.getOptions(this) || {};
+    const IMG_SRC_REGEX = /\bsrc="([^"]*)"/g;
+    const htmlResourceRoot = options.htmlResourceRoot;
     dot.templateSettings.selfcontained = true;
+    Object.assign(dot.templateSettings, options.dotSettings);
 
-    return `module.exports = ${dot.template(fs.readFileSync(this.resourcePath))}`;
+    result = `module.exports = ${dot.template(fs.readFileSync(this.resourcePath))}`;
+    result = result.replace(IMG_SRC_REGEX, (m, s1) => {
+        let code = '';
+
+        if (loaderUtils.isUrlRequest(s1, htmlResourceRoot)) {
+            const urlRequest = loaderUtils.urlToRequest(s1, htmlResourceRoot);
+            code = `';out+='src="';out+=require(${JSON.stringify(urlRequest)});out+='"`;
+        } else {
+            code = JSON.stringify(m);
+        }
+
+        return code;
+    });
+    return result;
 }
 
 module.exports = dotLoader;
-
