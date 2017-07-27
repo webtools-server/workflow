7da1808982b99f6552611b13998eaccd856fabc3
canye <canye.wu@jyblife.com>
2017-07-27 17:34:13 +0800

    test: jfet测试

--- packages/jfet/test/lib/index.test.js
+++ packages/jfet/test/lib/index.test.js
@@ -1,8 +1,7 @@
-
 const should = require('should');
 
 describe('Array', () => {
-    it('should return -1 when the value is not present', () => {
-        should(4).be.equal(4);
-    });
+  it('should return -1 when the value is not present', () => {
+    should(4).be.equal(4);
+  });
 });
