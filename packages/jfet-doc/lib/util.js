8eb0516e28d5665423d91daf2701cbb66b1b62dc
canye <canye.wu@jyblife.com>
2017-08-19 00:09:22 +0800

    feat: jfet-doc修改参数

--- packages/jfet-doc/lib/util.js
+++ packages/jfet-doc/lib/util.js
@@ -9,17 +9,18 @@ const fse = require('fs-extra');
 /**
  * 创建zip
  * @param {String} zipFile zip文件
+ * @param {String} zipName zip文件名
  * @param {String} fileDir 文件目录
  * @return {Promise}
  */
-function createZip(zipFile, fileDir) {
+function createZip(zipFile, zipName, fileDir) {
   return new Promise((resolve, reject) => {
     const outputZip = fse.createWriteStream(zipFile);
     const archive = archiver('zip');
 
     outputZip.on('close', () => {
       const size = archive.pointer();
-      console.log(chalk.green(`Zip: ${zipFile}`));
+      console.log(chalk.green(`Zip: ${zipName}`));
       console.log(chalk.green(`${Math.round(size / 1024)} total kb`));
       resolve(size);
     });
