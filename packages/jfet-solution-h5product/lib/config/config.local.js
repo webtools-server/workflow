9339dc5f1ad3df0ed62a1915d5946c9c50e7df4b
canye <canye.wu@jyblife.com>
2017-09-05 10:38:08 +0800

    feat: 增加产品站解决方案

---
+++ packages/jfet-solution-h5product/lib/config/config.local.js
@@ -0,0 +1,16 @@
+/**
+ * local config
+ */
+
+const defaultConfig = require('./config.default');
+
+module.exports = (config = {}) => {
+  const buildConfig = defaultConfig(config);
+
+  return Object.assign(buildConfig, {
+    defineConstants: Object.assign(buildConfig.defineConstants, {
+      'process.env.NODE_ENV': 'local'
+    }),
+    setDevTool: '#eval-source-map'
+  });
+};
---
+++ packages/jfet-solution-h5product/lib/config/config.prod.js
@@ -0,0 +1,15 @@
+/**
+ * prod config
+ */
+
+const defaultConfig = require('./config.default');
+
+module.exports = (config = {}) => {
+  const buildConfig = defaultConfig(config);
+
+  return Object.assign(buildConfig, {
+    defineConstants: Object.assign(buildConfig.defineConstants, {
+      'process.env.NODE_ENV': 'production'
+    })
+  });
+};
