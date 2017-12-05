6fba811574252fdbd38250a15eed6e9f0bc4ad5e
canye <canye.wu@jyblife.com>
2017-12-05 17:43:27 +0800

    feat: jfet-core修改package api地址

--- packages/jfet/lib/constant.js
+++ packages/jfet/lib/constant.js
@@ -16,7 +16,7 @@ module.exports = {
   // 解决方案前缀
   SOLUTION_PREFIX: ['@jyb/jfet-solution-', 'jfet-solution-'],
   // npm包信息接口
-  NPM_PACKAGE_API: 'http://npm.jtjr.com/',
+  NPM_PACKAGE_API: 'http://npm.jyb.com/',
   // npm搜索接口
-  NPM_SEARCH_API: 'http://npm.jtjr.com/-/search/@jyb%2Fjfet',
+  NPM_SEARCH_API: 'http://npm.jyb.com/-/search/@jyb%2Fjfet',
 };
